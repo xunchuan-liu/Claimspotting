bcbe9f566fc53bc08ac3f49271892118db94f6a0
andrewjjhuh <44470787+andrewjjhuh@users.noreply.github.com>
2020-01-21 16:12:51 -0600

    Delete Claimbuster.py

--- Claimbuster.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-response = requests.get("https://idir.uta.edu/factchecker/score_text/{Hello this is a test.}")
-temp = response.json()
-print(response)
-print(response.json())
-print(temp['results'][0]['score'])
-
