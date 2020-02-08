fb1f33825017d3d4d043d97f7fe8f94e3ad124d0
Xunchuan Liu <39782635+xunchuan-liu@users.noreply.github.com>
2020-02-08 13:54:37 -0600

    Delete Test.py

--- Scripts/Test.py
+++
@@ -1,9 +0,0 @@
-import re
-
-l = ["this is a dog", "this is a very fluffy dog", "this is a grey dog"]
-for s in l:
-    res = len(re.findall(r'\w+', s))
-    if res <= 5:
-        l.remove(s)
-
-print(l)
