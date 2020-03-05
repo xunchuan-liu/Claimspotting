f9da7322aec6240973defaaab445ecac62b781c6
therodrammer <39782635+therodrammer@users.noreply.github.com>
2020-03-05 13:26:47 -0600

    Changed driver code to align with new database connector. No more need for the pickle file.

--- Scripts/Run_Script.py
+++ Scripts/Run_Script.py
@@ -1,50 +1,13 @@
 from Scraper import Scraper
 from datetime import date, timedelta
-import pickle
+
+def main(day=date.today(), exists=None):	
+	scraper = Scraper(day, exists)	
+
+	scraper.select("HOUSE", 25, scraper.yesterday, "house_3.csv", True)
 
 
-def main(day=date.today().strftime("%Y-%m-%d"), createNew=None):
-	try:	
-		pickle_in = open("scraper.pickle","rb")
-		previous_scraper = pickle.load(pickle_in)
-	except:
-		pickle_in = None
-		previous_scraper = None
-	finally:
-		if pickle_in != None:
-			pickle_in.close()
-
-	if createNew == None:
-		createNew = (previous_scraper == None) or (previous_scraper.date != day)
-
-	if createNew:
-		print("Creating new scraper")
-		try:
-			scraper = Scraper(day)
-			pickle_out = open("scraper.pickle","wb")
-			pickle.dump(scraper, pickle_out)
-			pickle_out.close()
-		except:
-<<<<<<< HEAD
-			print("Error creating scraper")		
-=======
-			print("Error creating scraper")
->>>>>>> bc006d45812ccdb0dbbae982513607c67ab6be37
-			scraper = previous_scraper
-	else:
-		print("Today's scraper already exists")
-		scraper = previous_scraper
-
-
-<<<<<<< HEAD
-	samples = Scraper.sample(25, scraper.senateAll, "senate", True)
-	#samples = Scraper.sample(25, scraper.houseAll, "house", True)
-=======
-	samples = Scraper.sample(5, scraper.senateAll, True)
->>>>>>> bc006d45812ccdb0dbbae982513607c67ab6be37
-
-
-main()
+main(exists=False)
 
 	
 
