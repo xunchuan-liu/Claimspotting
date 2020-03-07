a26412084c32302ead0ef042e5cbc2cd4d9e79a8
therodrammer <39782635+therodrammer@users.noreply.github.com>
2020-03-06 18:49:42 -0600

    Driver code updated which now contains 1 function for scraping and 1 function for data analysis.

--- Scripts/Run_Script.py
+++ Scripts/Run_Script.py
@@ -1,14 +1,24 @@
 from Scraper import Scraper
-from datetime import date, timedelta
+from Results import Results
+from datetime import date, timedelta	
 
-def main(day=date.today(), exists=None):	
+## Runs the scraping for the day
+def Scraping(day=date.today(), exists=None):
 	scraper = Scraper(day, exists)	
 
-	scraper.select("HOUSE", 25, scraper.yesterday, "house_3.csv", True)
+	samples = scraper.select("SENATE", 25, scraper.yesterday, "senate_3.csv", True)
+
+## Runs the data analysis for the day
+def Analyze(fileName, createFiles=False):
+	results = Results(fileName)
+	results.get_responses()
+	results.combine(createFiles)
+	results.get_pearson(createFiles)	
+	results.Visualize()
 
 
-main(exists=False)
-
+Scraping()
+Analyze("./Test3/Results_3.csv")
 	
 
 
